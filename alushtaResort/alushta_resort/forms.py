23b3fbaba83deeba9cd7f9bc5248b8294f5b8141
AbibullaevAR <a.alim2019@mail.ru>
2024-05-16 17:37:31 +0300

    add Newsletter

--- alushtaResort/alushta_resort/forms.py
+++ alushtaResort/alushta_resort/forms.py
@@ -4,3 +4,6 @@ class BookingForm(forms.Form):
     name = forms.CharField(max_length=200)
     email = forms.EmailField()
     message = forms.CharField(max_length=240)
+
+class NewsletterForm(forms.Form):
+    email = forms.EmailField()
--- alushtaResort/alushta_resort/urls.py
+++ alushtaResort/alushta_resort/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import BookingFormView
+from .views import BookingFormView, NewsletterView
 
 app_name = 'alushta_resort'
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('booking', TemplateView.as_view(template_name='alushta_resort/booking.html')),
     path('rest', TemplateView.as_view(template_name='alushta_resort/rest.html')),
     path('apartment', TemplateView.as_view(template_name='alushta_resort/apartment.html')),
-    path('api/bookingForm', BookingFormView.as_view())
+    path('api/bookingForm', BookingFormView.as_view()),
+    path('api/newsletterForm', NewsletterView.as_view())
 ]
