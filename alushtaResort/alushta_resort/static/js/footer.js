9bb1a8f4db8bcea2ae2a7d23e55c4c125344499a
AbibullaevAR <a.alim2019@mail.ru>
2024-05-16 18:52:18 +0300

    add notification to subscrubie button

--- alushtaResort/alushta_resort/static/js/footer.js
+++ alushtaResort/alushta_resort/static/js/footer.js
@@ -2,15 +2,30 @@
 
     const subscribeButtonElem = document.querySelector('.footer__subscribe-button');
     const subscribeEmailElem = document.querySelector('.footer__subscribe-email');
+    const notificationErrorElem = document.querySelector('.footer__subscribe-notification_error');
+    const notificationSuccessElem = document.querySelector('.footer__subscribe-notification_success');
 
     subscribeButtonElem.addEventListener('click', () => {
         const subscribeForm = new FormData();
         subscribeForm.append('email', subscribeEmailElem.value);
 
+        subscribeButtonElem.innerHTML = `<div class="loader"></div>`
+
+        notificationErrorElem.classList.remove('footer__subscribe-notification_visable');
+        notificationSuccessElem.classList.remove('footer__subscribe-notification_visable');
+
         fetch('http://127.0.0.1:8000/api/newsletterForm', {
             method: 'POST',
             body: subscribeForm
         }).then(request => {
+            if (request.status >= 400) {
+                notificationErrorElem.classList.add('footer__subscribe-notification_visable');
+            } else {
+                notificationSuccessElem.classList.add('footer__subscribe-notification_visable');
+            }
+
+            subscribeButtonElem.innerHTML = 'Ok'
+            subscribeEmailElem.value = ''
         })
     })
 }())
